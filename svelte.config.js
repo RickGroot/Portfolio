a1b4440393aaf9627124e0490805b7f0ed5fe2d5
Rick Groot <RickGroot41@gmail.com>
2023-02-12 12:07:44 +0100

    sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { build } from 'vite';
+
+build.outDir = 'dist';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +11,16 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		// target: '#svelte',
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+			// precompress: false,
+			// strict: true
+		})
 	}
 };
 
